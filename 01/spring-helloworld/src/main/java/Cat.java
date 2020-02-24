6ae32fe6d5ef928cd6943f5e8fb3be68209de00b
YuraForward <iury.fedoroff2018@yandex.ru>
2020-02-24 19:18:57 +0300

    Signed-off-by: YuraForward <iury.fedoroff2018@yandex.ru>

---
+++ 01/spring-helloworld/src/main/java/Cat.java
@@ -0,0 +1,20 @@
+
+/*
+ *
+ *@Data 24.02.2020
+ *@autor Fedorov Yuri
+ *@project spring-helloworld
+ *
+ */
+
+public class Cat {
+    private String message;
+    public Cat() { }
+    public Cat(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
